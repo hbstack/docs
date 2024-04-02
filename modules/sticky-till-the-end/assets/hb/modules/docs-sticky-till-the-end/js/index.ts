323b424698e9f2bbef249c46bb378dc86c5bff92
razonyang <razonyang@gmail.com>
2024-04-02 19:41:34 +0800

    feat: add the sticky-till-the-end module

---
+++ modules/sticky-till-the-end/assets/hb/modules/docs-sticky-till-the-end/js/index.ts
@@ -0,0 +1,3 @@
+import { attachOn } from '../../footer-observer/js/handler';
+
+attachOn ('.hb-docs-nav', '.hb-docs-doc-toc')  
---
+++ modules/sticky-till-the-end/go.mod
@@ -0,0 +1,3 @@
+module github.com/hbstack/docs/modules/sticky-till-the-end
+
+go 1.19
